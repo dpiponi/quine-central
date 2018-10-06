0f9c25f12fe3ffbe9bfc7a842481c214e3e5b13b
Dan Piponi <174337+dpiponi@users.noreply.github.com>
2018-10-05 18:31:16 -0700

    Regression test

---
+++ test.sh
@@ -0,0 +1,51 @@
+#!/bin/bash -e
+
+HASKELL() {
+  stack runghc $1
+}
+RUBY() {
+  ruby $1
+}
+PERL() {
+  perl $1
+}
+PYTHON() {
+  python $1
+}
+JAVA() {
+  cp $1 quine.java
+  javac quine.java
+  java quine
+}
+C() {
+  cc -Wno-format-security -o quine $1
+  ./quine
+}
+RUST() {
+  rustc -o quine $1
+  ./quine
+}
+OCAML() {
+  ocaml $1
+}
+SWIFT() {
+  swift $1
+}
+
+declare -i n=9
+declare -a langs=(HASKELL RUBY PERL C PYTHON JAVA RUST OCAML SWIFT HASKELL) 
+declare -a exts=(hs rb pl c py java rs ocaml swift hs) 
+
+# Create the initial quine
+HASKELL quineCentral.hs > quine0.${exts[0]}
+
+# Loop through languages
+for i in $(seq 0 $((n-1)))
+do
+  echo ${langs[$i]} quine$((i)).${exts[$i]} ">" quine$((i+1)).${exts[$i+1]}
+  ${langs[$i]} quine$((i)).${exts[$i]} > quine$((i+1)).${exts[$i+1]}
+done
+
+# Final test
+echo diff quine0.hs quine$((n)).hs
+diff quine0.${exts[0]} quine$((n)).hs
